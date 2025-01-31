87b3a41dd0de94fad220c7d54ae13401bee0b718
Harold Ekpo <42976021+KiCodes@users.noreply.github.com>
2025-01-31 15:23:21 +0100

    Update otp_widget.dart

--- lib/src/widgets/otp_widget.dart
+++ lib/src/widgets/otp_widget.dart
@@ -42,7 +42,7 @@ class _OtpWidgetState extends BaseState<OtpWidget> {
                   textAlign: TextAlign.center,
                   style:  TextStyle(
                     fontWeight: FontWeight.w500,
-                    color: context.textTheme.titleLarge?.color,
+                    color: Theme.of(context).textTheme.titleLarge?.color,
                     fontSize: 15.0,
                   ),
                 ),
